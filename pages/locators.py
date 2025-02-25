51cf67feae2a019e9866f0fb60e924b44c396e1b
Mikhail <maxtax1369@gmail.com>
2025-02-25 14:04:16 +0300

    stepik step 4.3.4 parametrize test_guest_can_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,15 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    ALERT_SUCCESS_PRODUCT_NAME = (By.CSS_SELECTOR, '.alert-success > div > strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
+    BASKET_PRICE = (By.CSS_SELECTOR, '.basket-mini')
---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        button.click()
+
+    def should_be_product_add_to_cart(self):
+        self.should_be_message_product_add_to_cart()
+        self.should_be_basket_price_equal_added_product()
+
+    def should_be_message_product_add_to_cart(self):
+        product_name_text = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        success_alerts = self.browser.find_elements(*ProductPageLocators.ALERT_SUCCESS_PRODUCT_NAME)
+
+        is_match = False
+        alert_text_list = []
+        for success_alert in success_alerts:
+            alert_text = success_alert.text
+            alert_text_list.append(alert_text)
+            if product_name_text == alert_text:
+                is_match = True
+                break
+
+        assert is_match, f'Product name "{product_name_text}" is not equal to "{alert_text_list}"'
+
+    def should_be_basket_price_equal_added_product(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+
+        assert product_price in basket_price, f'Product price "{product_price}" is not contains basket price "{basket_price}"'
